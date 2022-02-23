d7f0d14acaaba463c5e7e49d51cea87de65b2711
jihoon <rec8730@gmail.com>
2022-02-23 22:23:28 +0900

    2월23일 삼항연산자 if문, Component문법 추가

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,12 +6,21 @@ import './App.css';
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','빙다리 핫바지']);
   let [따봉, 따봉변경] = useState(0);
+  
+  // UI의 상태 보이기 / 숨기기 등
+  let [modalShow, modal변경] = useState(false);
+
+  // function 제목바꾸기(){
+  //   let newArray = [...글제목];
+  //   newArray[0] = '여자코트 추천';
+  //   글제목변경( newArray );
+  // }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ () => {글제목변경( 글제목[0] = ['여자 코트 추천'])}}>버튼</button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
@@ -27,8 +36,28 @@ function App() {
         <p>2월 22일 발행</p>
         <hr/>
       </div>
+      <button onClick = { () => {modal변경(true)}}>열기 / 닫기</button>
+
+      {/* 삼항연산자 if문 */}
+      {
+        modalShow === true
+        ? <Modal />
+        : null
+      }
+
     </div>
   );
+}
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>
+        제목
+      </h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
